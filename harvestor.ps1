3fef0765326a940e1eaeed7e30523c19ab718320
M007 <87243259+michal-kopl@users.noreply.github.com>
2023-03-28 21:56:10 +0200

    Update harvestor.ps1
    anonymization of the path on the NB, before its publication

--- harvestor.ps1
+++ harvestor.ps1
@@ -1,5 +1,5 @@
-$OutputFile = "C:\_DEV\Akcenta\emails.txt" #Resulting file with harvested info
-$SiteMapFile = "C:\_DEV\Akcenta\sitemap.xml" #Source file with URL addresses to check and harvest the data from
+$OutputFile = "C:\_DEV\emails.txt" #Resulting file with harvested info
+$SiteMapFile = "C:\_DEV\sitemap.xml" #Source file with URL addresses to check and harvest the data from
 Get-Content -Path $SiteMapFile | ForEach-Object -Process {
     $URL = $_;
     $Request = Invoke-WebRequest $URL;$Request.AllElements | Where-Object {$_.InnerHtml -like "*@*"} | Select-Object innerText -Unique | Where-Object{$_.InnerText -like "*@*.*"} | Select-Object -ExpandProperty innerText >> $OutputFile
